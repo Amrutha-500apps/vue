434c6f8676d7a617ad950ae006dc34518cdc3cad
Amrutha-500apps <amrutha.kothapalli@500apps.com>
2022-05-10 21:22:34 +0530

    exam

---
+++ src/components/Ques5.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+<b-form-input v-model="product_name" placeholder="product name" required></b-form-input><br><br>
+<b-form-input v-model="product_price" placeholder="product price" required></b-form-input><br><br>
+<b-form-input v-model="product_category" placeholder="product category" required></b-form-input><br><br>
+<b-button @click=add() >Add</b-button><br><br>
+<b-table striped hover :items="products" :fields="fields">
+</b-table>
+</div>
+</template>
+<script>
+export default {
+name:"producT",
+data() {
+return {
+products: [
+{ name: "fridge", price: 30000, category: 'electronics' },
+{ name: "books", price: 30, category: 'stationary' },
+{ name: "AC", price: 30000, category: 'electronics' },
+{ name: "pens", price: 20, category: 'stationary' },
+],
+fields:["name","price","category"],
+product_name:'',
+product_price:'',
+product_category:''
+}
+},
+methods:{
+add(){
+this.products.push({name:this.product_name, price:this.product_price ,category:this.product_category})
+}
+}
+}
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '../components/HelloWorld'
-import pro1 from '../components/pro1'
-import calls from '../components/calls'
+import Ques5 from '../components/Ques5'
  Vue.use(Router)
 const routes = [
     {
@@ -11,15 +10,9 @@ const routes = [
         component:HelloWorld
     },
     {
-        path:'/pro1',
-        name:'Project',
-        component:pro1
-    },
-
-    {
-        path:'/calls',
-        name:'sampleApis',
-        component:calls
+        path:'/Ques5',
+        name:'App',
+        component:Ques5
     },
 ]
 
